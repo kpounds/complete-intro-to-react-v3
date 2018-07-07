cc40190e9228372818730caeda0454ef88bf50a7
Keith Pounds <keith.pounds@autozone.com>
2018-07-06 22:24:33 -0500

    Adding base files for project

---
+++ js/ClientApp.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const MyTitle = function (props) {
+  return (
+      React.createElement('div', null,
+          React.createElement('h1', { style: { color: props.color } }, props.title)
+      )
+  )
+}
+
+const MyFirstComponent = function () {
+  return React.createElement('div', null, 
+      React.createElement(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+      React.createElement(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
+      React.createElement(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
+      React.createElement(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+  )
+}
+
+render(
+  React.createElement(MyFirstComponent),
+  document.getElementById('app')
+)
